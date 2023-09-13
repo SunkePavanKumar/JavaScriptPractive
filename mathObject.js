391bf845cc60c0dcffe01e7a534ac34bc1a15436
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-09-13 21:06:32 +0530

    COM : Math object method JS

---
+++ mathObject.js
@@ -0,0 +1,15 @@
+let x;
+x = 4
+console.log(Math.sqrt(x))
+
+console.log(Math.abs(-10.78))  // Will return the absolute  value even the number is negative 
+console.log(Math.round(89.4)) // how the normal round functionality works in the same way
+console.log(Math.ceil(33.2)) // return the upper bound
+console.log(Math.random())  // returns the random decimal from 0 to 1
+let y = Math.floor(Math.random()*10 +1)
+
+console.log(y)
+console.log(Math.floor(10.78))
+
+// Math.min
+//Math.max
