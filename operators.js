e868efb75bb2c115b61f1c435f8bec5bbd28c84a
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-09-13 17:54:58 +0530

    COM : operatos complete and coercion WIP

---
+++ operators.js
@@ -0,0 +1,38 @@
+// Arthematic operations
+
+let x;
+x = 5 +5
+x = 5 *5
+x = 5 -5
+x = 5 %5
+x = 5 /5
+
+//concatination
+
+x = "pavan" + "Kumar";
+
+// Exponent operator
+
+x = 2**3
+
+// Increment operator
+
+x = x++
+
+// Decrement operator
+
+x = x--
+
+// Assignment operatiors
+
+// comparation operators
+/**
+ * 
+ *  ==  return true if the values is similar. It doesn't check the type
+ *  === checks the type as well an the value
+ *  !== and !==  one checks only the values whereas the other checks both
+ * > ,< , >=
+ */
+
+
+console.log(x)
