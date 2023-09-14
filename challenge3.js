f0ca56f32c841677282557ca9f23ed890f3392bf
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-09-14 11:57:43 +0530

    COM : array challenge2

---
+++ challenge3.js
@@ -0,0 +1,16 @@
+
+// challenge one 
+// const array = [1,2,3,4,5]
+// array.push(6)
+// array.unshift(0)
+// array.reverse()
+// console.log(array)
+
+//challenge 2
+
+// const arr1 = [1,2,3,4,5]
+// const arr2 = [5,6,7, 8, 9,10]
+// arr1.pop()
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
